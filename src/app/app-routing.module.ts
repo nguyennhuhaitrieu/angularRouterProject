7d9520a0f535a7b072b6ddfe356f2e8eaff9490a
nguyennhuhaitrieu <nguyennhuahitrieu@gmail.com>
2019-03-17 11:55:15 +0700

    angular Router Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactComponent } from './project-one/contact.component';
+import { HoneComponent } from './project-one/home.component';
+import { NotFoundComponent } from './project-one/not-found.component';
+import { AboutComponent } from './project-one/about.component';
+
+const appRoutes: Routes = [
+  { path: '',             component: HoneComponent},
+  { path: 'home',         component: HoneComponent },
+  { path: 'contact',      component: ContactComponent },
+  { path: 'about',        component: AboutComponent },
+  { path: 'about/web',    component: AboutComponent },
+  { path: '**',           component: NotFoundComponent }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [ RouterModule.forRoot(appRoutes) ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ContactComponent } from './project-one/contact.component';
+import { HoneComponent } from './project-one/home.component';
+import { NotFoundComponent } from './project-one/not-found.component';
+import { AboutComponent } from './project-one/about.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HoneComponent,
+    NotFoundComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
